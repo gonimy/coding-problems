b8e9d2efd0dc1aceb10e022c9b68f75cb53af806
gonimy <69145711+gonimy@users.noreply.github.com>
2020-08-05 14:32:05 +0200

    Create Non-decreasing Array.cpp

---
+++ leetcode/Non-decreasing Array.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+public:
+	bool checkPossibility(vector<int>& nums) {
+		for (int i = 0; i < nums.size() - 1; i++) {
+			if (nums[i] > nums[i + 1]) {
+				int temp = nums[i];
+				nums[i] = nums[i + 1];
+				if (is_sorted(nums.begin(), nums.end()))
+					return true;
+				nums[i + 1] = nums[i] = temp;
+				if (is_sorted(nums.begin(), nums.end()))
+					return true;
+				return false;
+			}
+		}
+		return true;
+	}
+};
