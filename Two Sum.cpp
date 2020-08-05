a3ca4ef339a17ea072d1b5c3e7a06beb863ed961
gonimy <69145711+gonimy@users.noreply.github.com>
2020-08-05 12:38:52 +0200

    Create Two Sum.cpp

---
+++ Two Sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+vector<int> twoSum(vector<int> &nums, int target)
+{
+    vector<int> res;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int diff = target - nums[i];
+
+                  if(find(nums.begin()+i+1, nums.end(), diff) != nums.end()){
+  
+            auto it = find(nums.begin()+i+1, nums.end(), diff) ;
+            int j = distance(nums.begin(), it);
+            res.push_back(i);
+            res.push_back(j);
+        }
+    }
+    return res;
+}
+
+
+};
