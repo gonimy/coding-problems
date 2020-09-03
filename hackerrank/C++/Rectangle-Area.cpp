da48045950c90c7f501bda8dc88b3144eae09b6d
gonimy <mhmodgonim96@gmail.com>
2020-09-03 22:30:25 +0200

    adding HR problems

---
+++ hackerrank/C++/Rectangle-Area.cpp
@@ -0,0 +1,31 @@
+
+/*
+ * Create classes Rectangle and RectangleArea
+ */
+class Rectangle
+{
+    protected:
+       int width;
+       int height;
+    
+    public:
+    void display() {
+        
+        cout<<width<<" "<<height;
+        
+         }
+};
+
+class RectangleArea : public Rectangle {
+        
+        public:
+        void read_input(){
+            cin>>width>>height;
+        }
+        
+        void display() {
+            cout<<endl<<width*height;
+        }
+        
+    };
+
